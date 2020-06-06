70817a024b8b07e5934dbbb90e73861bce068b89
mateusfb <mateusfirminob@gmail.com>
2020-06-06 15:01:28 -0300

    Unificando dicionarios de cluster

--- summarizer/src/summarizer.py
+++ summarizer/src/summarizer.py
@@ -83,15 +83,13 @@ def dict_from_json(src):
 
 def make_dict(csv_file, cluster_list=None):
     cluster_dict = count_qualis_occurances_for_programs(csv_file)
+    selected_clusters = cluster_dict
 
-    if cluster_list is None:
-        selected_clusters = cluster_dict
-    else:
+    if cluster_list is not None:
         selected_clusters = string_to_list(cluster_list, separator='-')
 
-    for cluster in selected_clusters:
-        filename = "../results/" + file_name_from_path(csv_file) + "_" + cluster + ".json"
-        dict_to_json(filename, cluster_dict[cluster])
+    filename = "../results/" + file_name_from_path(csv_file) + ".json"
+    dict_to_json(filename, {key: value for key, value in cluster_dict.items() if key in selected_clusters})
 
 
 def make_values_dict_from_keys(dictionary, keys):
@@ -104,10 +102,10 @@ def make_values_dict_from_keys(dictionary, keys):
     return values_dict
 
 
-def plot_program(dict_src, program_code_list_str, image_name=None):
+def plot_program(dict_src, program_code_list_str, cluster, image_name=None):
     program_code_list = string_to_list(program_code_list_str, separator="-")
     subdict = {key: value for key, value in dict_from_json(
-        dict_src).items() if key in program_code_list}
+        dict_src)[cluster].items() if key in program_code_list}
     count_dict = make_values_dict_from_keys(subdict, ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C/NI/NP"])
 
     fig, ax = plt.subplots()
@@ -142,12 +140,12 @@ def plot_program(dict_src, program_code_list_str, image_name=None):
             dict_src) + "_" + program_code_list_str + ".png"
     fig.savefig(image_name)
 
-def plot_n_best(dict_src, n_str, compare_to=None, image_name=None):
+def plot_n_best(dict_src, n_str, cluster, compare_to=None, image_name=None):
     n = int(n_str)
     program_code_list_str = "["
     count = 0
 
-    for program in dict_from_json(dict_src):
+    for program in dict_from_json(dict_src)[cluster]:
         if count == n:
             break
 
@@ -156,18 +154,18 @@ def plot_n_best(dict_src, n_str, compare_to=None, image_name=None):
         else:
             program_code_list_str += program 
             if compare_to is not None:
-                program_code_list_str += compare_to + ']'
+                program_code_list_str += '-' + compare_to + ']'
             else:
                 program_code_list_str += ']'
 
         count += 1
 
-    plot_program(dict_src, program_code_list_str, image_name)
+    plot_program(dict_src, program_code_list_str, cluster, image_name)
 
-def plot_compare(dict_src, program_code_list_str, image_name=None):
+def plot_compare(dict_src, program_code_list_str, cluster, image_name=None):
     program_code_list = string_to_list(program_code_list_str, separator="-")
     subdict = {key: value for key, value in dict_from_json(
-        dict_src).items() if key in program_code_list}
+        dict_src)[cluster].items() if key in program_code_list}
     count_dict = make_values_dict_from_keys(subdict, ["A1/A2/A3/A4", "B1/B2/B3/B4", "C/NI/NP"])
 
     fig, ax = plt.subplots()
@@ -200,3 +198,8 @@ def plot_compare(dict_src, program_code_list_str, image_name=None):
         image_name = "../results/" + file_name_from_path(
             dict_src) + "_" + program_code_list_str + "_compare_" + ".png"
     fig.savefig(image_name)
+
+
+#make_dict("../datasets/Particao_k3_c1.csv")
+#plot_n_best("../results/Particao_k3_c1.json", 3, "cluster1", "23001011030P1")
+#plot_compare("../results/Particao_k3_c1.json", "[23001011010P0-23001011031P8-23001011020P6]", "cluster1")
