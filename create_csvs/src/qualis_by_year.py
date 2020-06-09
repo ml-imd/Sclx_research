645fd45d7f777e750e19605dca97963291ebbf26
ArthurGorgonio <gorgonioarthur@gmail.com>
2020-06-09 13:30:30 -0300

    creating a menu to show how to use and comment the functions

--- create_csvs/src/qualis_by_year.py
+++ create_csvs/src/qualis_by_year.py
@@ -1,80 +1,139 @@
-import pandas as pd
-
+from pandas import read_csv
+from pandas import DataFrame
 
 def create_author():
-    d = {'A1': 0,
-         'A2': 0,
-         'A3': 0,
-         'A4': 0,
-         'B1': 0,
-         'B2': 0,
-         'B3': 0,
-         'B4': 0,
-         'C/NI/NP': 0}
-    autor_obj = {'id_autor': 0,
-                 'nome_normalizado': '',
-                 'h_index': 0,
-                 'num_colab_inter': 0,
-                 'num_person_colab_inter': 0,
-                 'docente_ppg': 0,
-                 'd1': d.copy(),
-                 'd2': d.copy(),
-                 'd3': d.copy(),
-                 'd4': d.copy()
-                 }
-    return autor_obj
+    """
+    Create a structure to represent an author
+
+    :return: a new dict that represents an author
+    """
+    author_obj = {
+        'id_autor': 0,
+        'nome_normalizado': '',
+        'h_index': 0,
+        'num_colab_inter': 0,
+        'num_person_colab_inter': 0,
+        'docente_ppg': 0,
+        'd1_A1': 0,
+        'd1_A2': 0,
+        'd1_A3': 0,
+        'd1_A4': 0,
+        'd1_B1': 0,
+        'd1_B2': 0,
+        'd1_B3': 0,
+        'd1_B4': 0,
+        'd1_C/NI/NP': 0,
+        'd2_A1': 0,
+        'd2_A2': 0,
+        'd2_A3': 0,
+        'd2_A4': 0,
+        'd2_B1': 0,
+        'd2_B2': 0,
+        'd2_B3': 0,
+        'd2_B4': 0,
+        'd2_C/NI/NP': 0,
+        'd3_A1': 0,
+        'd3_A2': 0,
+        'd3_A3': 0,
+        'd3_A4': 0,
+        'd3_B1': 0,
+        'd3_B2': 0,
+        'd3_B3': 0,
+        'd3_B4': 0,
+        'd3_C/NI/NP': 0,
+        'd4_A1': 0,
+        'd4_A2': 0,
+        'd4_A3': 0,
+        'd4_A4': 0,
+        'd4_B1': 0,
+        'd4_B2': 0,
+        'd4_B3': 0,
+        'd4_B4': 0,
+        'd4_C/NI/NP': 0
+    }
+    return author_obj
 
 
-def split_autor(autor_list):
-    autor_list = autor_list[1:-1]
-    return autor_list.split('-')
+def split_author(author_list):
+    """
+    Spliting an author string to an author list
+
+    :param author_list: a string that refers the id of the authors to be splited.
+    :return: a list with id authors
+    """
+    author_list = author_list[1:-1]
+    return author_list.split('-')
 
 
-def get_location_in_dict(ano):
-    if ano >= 2020:
+def get_location_in_dict(year):
+    """
+    Choosing the correct location according the pulbication year of the paper.
+
+    :param year: the publication year of the corresponding paper.
+    :return: a correspondent decade of the paper
+    """
+    if year >= 2020:
         s = 'd4'
-    elif ano >= 2010:
+    elif year >= 2010:
         s = 'd3'
-    elif ano >= 2000:
+    elif year >= 2000:
         s = 'd2'
-    elif ano >= 1990:
+    elif year >= 1990:
         s = 'd1'
     else:
         s = ''
     return s
 
 
-def count_qualis_occurances_for_autores(src):
-    dataframe = pd.read_csv(src)
-    autor_dict = {}
+def count_qualis_occurances_for_authors(src):
+    """
+    Save in a csv file a processed data frame with only one "programa" per
+    instance
+
+    :param dataIn: a string that refers the name of the data frame which
+        should be processed.
+    :param dataOut: the output name of the data frame.
+    :return: a dictionary with esch position represents an author with all
+        publications of the received file.
+    """
+    dataframe = read_csv(src)
+    author_dict = {}
     for i in dataframe.index:
         qualis = dataframe['qualis'][i]
-        ano = dataframe['ano'][i]
-        decada = get_location_in_dict(ano)
-        autor_id_list = split_autor(dataframe['autores'][i])
+        year = dataframe['ano'][i]
+        decada = get_location_in_dict(year)
+        author_id_list = split_author(dataframe['autores'][i])
         if decada != '':
-            for autor_id in autor_id_list:
-                if autor_id not in autor_dict.keys():
-                    autor_dict[autor_id] = create_author()
-                    autor_dict[autor_id]['id_autor'] = autor_id
+            for author_id in author_id_list:
+                if author_id not in author_dict.keys():
+                    author_dict[author_id] = create_author()
+                    author_dict[author_id]['id_autor'] = author_id
                     # Call função
-                    metrics = autor_outras_metricas(int(autor_id))
+                    metrics = author_other_metrics(int(author_id))
                     if metrics != '':
-                        autor_dict[autor_id]['num_person_colab_inter'] = \
+                        author_dict[author_id]['num_person_colab_inter'] = \
                             metrics.pop()
-                        autor_dict[autor_id]['num_colab_inter'] = metrics.pop()
-                        autor_dict[autor_id]['h_index'] = metrics.pop()
-                        autor_dict[autor_id]['nome_normalizado'] = metrics.pop()
-                        autor_dict[autor_id]['docente_ppg'] = 1
-                autor_dict[autor_id][decada][qualis] += 1
-    return autor_dict
+                        author_dict[author_id]['num_colab_inter'] = metrics.pop()
+                        author_dict[author_id]['h_index'] = metrics.pop()
+                        author_dict[author_id]['nome_normalizado'] = metrics.pop()
+                        author_dict[author_id]['docente_ppg'] = 1
+                author_dict[author_id][decada + '_' + qualis] += 1
+    return author_dict
 
 
-def autor_outras_metricas(autor_id):
-    data = pd.read_csv('Pesquisadores_UFRN.csv')
-    aux = list(data.id_autor)
+def author_other_metrics(author_id):
+    """
+    Get the author's name, h_index, international colaboration and international
+    colaborators if his is a researcher at the UFRN.
+
+    :param author_id: the id of the author to check if his is a researcher at
+    the UFRN.
+    :return: the respective metrics if his exists in the csv
+    """
+    data = read_csv('autores_ufrn.csv')
+    aux = list(data.id_author)
     try:
-        pos = aux.index(autor_id)
+        pos = aux.index(author_id)
         nome_norm = data.nome_normalizado[pos]
         h_index = data.h_index[pos]
         colaboracoes_inter = data.colaborações_internacionais[pos]
@@ -87,3 +146,18 @@ def autor_outras_metricas(autor_id):
         return metrics.copy()
     except ValueError:
         return ''
+
+
+def run(data_in, data_out):
+    """
+    Save in a csv file a processed dictionary with only one "programa" per
+    instance
+
+    :param dataIn: a string that refers the name of the data frame which
+    should be processed.
+    :param dataOut: the output name of the data frame.
+    :return: a void return
+    """
+    print("called Qualis By Year")
+    data_dict = count_qualis_occurances_for_authors(data_in)
+    DataFrame.from_dict(data_dict, orient='index').to_csv(data_out, index=False)
--- create_csvs/src/run.py
+++ create_csvs/src/run.py
@@ -1,15 +1,54 @@
-from partition import run
 import sys
+
+
+import papers_data_creation
+import qualis_by_year
+import split_program
+
+
+def name_the_file(filename, dataIn):
+    """
+    Modify the name of the file passed as a param'
+
+    :param filename: the parameter received in command line
+    :param dataIn: the name of the file received by command line
+    :return: the location of the file
+    """
+    try:
+        length = len(filename.split('/'))
+        if length == 1:
+            file_location = '../results/' + filename
+        elif length == 2:
+            file_location = file_location + dataIn + 'Output.csv'
+        else:
+            file_location = filename
+    except NameError:
+        file_location = file_location + dataIn + 'Output.csv'
+    return file_location
 
 if __name__ == "__main__":
     path = '.'
-    print("Try to start main\n")
-
-    if len(sys.argv) == 2:
-        run(sys.argv[1], sys.argv[1].split('.')[0] + "Output.csv")
-    elif len(sys.argv) == 3:
-        run(sys.argv[1], sys.argv[2])
+    if sys.argv[1] == '-h' or sys.argv[1] == '--help' or sys.argv[1] == '-H':
+        print('This file require 3 parameters, that follows:')
+        print('The first represents witch file do you want to execute, in order:')
+        print('\t 1 - papers_data_creation.py this file generate a new csv,',
+        'where each line represents a paper.')
+        print('\t 2 - split_program this file generate a new csv, where each', 
+        'line represents a "programa".')
+        print('\t 3 - qualis_by_year this file create a new csv file, where',
+        'each line represent a researcher.')
+        print('\n The second parameter represent the input file and the location')
+        print('\n The last parameter represents the output file and the location')
     else:
-        print("bad input must be two or three!!")
-        sys.exit(1)
-    print("Finish!!\n")
+        dataIn = sys.argv[2]
+        dataOut = name_the_file(sys.argv[3], sys.argv[1].split('.')[0])
+        num = int(sys.argv[1])
+        if num == 1:
+            papers_data_creation.run(dataIn, dataOut)
+        elif num == 2:
+            split_program.run(dataIn, dataOut)
+        elif num == 3:
+            qualis_by_year.run(dataIn, dataOut)
+        else:
+            print('cannot be excuted')
+            sys.exit(1)
