897fe8ea7306f197a8fa1fd2b8a9f5efc146c0c0
Song Jong Márcio Simioni da Costa <janeirode97@gmail.com>
2020-06-26 02:08:53 -0300

    Frame Tkinter de leitura do CSV e criação do dicionário JSON

--- summarizer/src/summarizer.py
+++ summarizer/src/summarizer.py
@@ -1,9 +1,11 @@
+import os.path
+import tkinter as tk
+from tkinter import filedialog
 import numpy as np
 import pandas as pd
-from ntpath import split, basename
 import matplotlib.pyplot as plt
+from ntpath import split, basename
 import json
-import sys
 
 
 def string_to_list(list_str, separator=',', delimiter='[]'):
@@ -13,14 +15,12 @@ def string_to_list(list_str, separator=',', delimiter='[]'):
 def file_name_from_path(path):
     head, tail = split(path)
     filename = tail or basename(head)
-
-    #removing filename extension
+    # removing filename extension
     if "." in filename:
         i = -1
-        while(filename[i] != '.'):
+        while filename[i] != '.':
             i -= 1
         filename = filename[:i]
-
     return filename
 
 
@@ -32,15 +32,14 @@ def sort_programs_dict(cluster_dict, sort_by):
 
 
 def count_qualis_occurances_for_programs(src):
-    dataframe = pd.read_csv(src)
+    df = pd.read_csv(src)
     cluster_dict = {}
     programs_list = []
-
-    for i in dataframe.index:
-        qualis = dataframe["qualis"][i]
-        cluster = dataframe["Cluster"][i]
+    for i in df.index:
+        qualis = df["qualis"][i]
+        cluster = df["Cluster"][i]
         programs_list = string_to_list(
-            dataframe["programas"][i], separator='-')
+            df["programas"][i], separator='-')
         for program in programs_list:
             if cluster not in cluster_dict:
                 cluster_dict[cluster] = {}
@@ -57,21 +56,18 @@ def count_qualis_occurances_for_programs(src):
                                                   "A1/A2/A3/A4": 0,
                                                   "B1/B2/B3/B4": 0,
                                                   "total": 0}
-
             cluster_dict[cluster][program][qualis] += 1
             cluster_dict[cluster][program]["total"] += 1
             if qualis in ("A1", "A2", "A3", "A4"):
                 cluster_dict[cluster][program]["A1/A2/A3/A4"] += 1
             elif qualis in ("B1", "B2", "B3", "B4"):
                 cluster_dict[cluster][program]["B1/B2/B3/B4"] += 1
-
     return cluster_dict
 
 
 def dict_to_json(filename, dictionary):
-    #converting dictionary into a json string
+    # converting dictionary into a json string
     json_dict = json.dumps(dictionary)
-
     with open(filename, "w") as file:
         file.write(json_dict)
 
@@ -79,30 +75,26 @@ def dict_to_json(filename, dictionary):
 def dict_from_json(src):
     with open(src) as json_file:
         dictionary = json.load(json_file)
-
         return dictionary
 
 
 def make_dict(csv_file, cluster_list=None, sort_by="A1/A2/A3/A4"):
     cluster_dict = sort_programs_dict(count_qualis_occurances_for_programs(csv_file), sort_by)
     selected_clusters = cluster_dict
-
-    #checking if a list of clusters was specified
+    # checking if a list of clusters was specified
     if cluster_list is not None:
         selected_clusters = string_to_list(cluster_list, separator='-')
-
-    filename = "../results/" + file_name_from_path(csv_file) + ".json"
+    filename = "./" + file_name_from_path(csv_file) + ".json"
     dict_to_json(filename, {key: value for key, value in cluster_dict.items() if key in selected_clusters})
 
 
 def make_values_dict_from_keys(dictionary, keys):
     values_dict = {k: [] for k in keys}
-    
     for key in dictionary:
         for value in values_dict:
             values_dict[value].append(dictionary[key][value])
-
     return values_dict
+
 
 def label_bar(ax, bars):
     for bar in bars:
@@ -112,6 +104,7 @@ def label_bar(ax, bars):
                     xytext=(0, 3),
                     textcoords="offset points",
                     ha='center', va='bottom')
+
 
 def get_program_name(program):
     programs = {
@@ -206,51 +199,46 @@ def get_program_name(program):
         '31075010001P2': 'MATEMÁTICA EM REDE NACIONAL',
         '53001010073P0': 'CONTABILIDADE'
     }
-
     return programs[program]
 
-def plot(subdict, program_code_list, count_dict, title, program_name, image_name, template="program", comparing=False, auto_label=False, cluster=False):
-    #defining some specific attributes based on what template is being used for the graphs
+
+def plot(subdict, program_code_list, count_dict, title, program_name, image_name, template="program", comparing=False,
+         auto_label=False, cluster=False):
+    # defining some specific attributes based on what template is being used for the graphs
     if template == "program":
         bar_width = 0.08
         colors = ["#54c73a", "#abd216", "#ccce0f", "#f0f200",
                   "#ffce00", "#ff9a00", "#ff6700", "#ff3300", "#ff0000"]
         tick_translate = 4
-        axvline_translate = 10*bar_width + bar_width/3
+        axvline_translate = 10 * bar_width + bar_width / 3
     elif template == "compare":
         bar_width = 0.15
         colors = ["#d6af36", "#a7a7ad", "#a77044"]
         tick_translate = 1
-        axvline_translate = 4*bar_width + bar_width/3
-    
+        axvline_translate = 4 * bar_width + bar_width / 3
     fig, ax = plt.subplots()
     x = np.arange(len(program_code_list))
     multiplier = 0
-
     for key in count_dict:
         bar = ax.bar(x + bar_width * multiplier, count_dict[key], width=bar_width, label=key,
-               color=colors[multiplier])
+                     color=colors[multiplier])
         if auto_label:
-            #labeling each bar
+            # labeling each bar
             label_bar(ax, bar)
         multiplier += 1
-
-    #plotting vertical lines between each program or group
+    # plotting vertical lines between each program or group
     j = 0
     for i in range(len(program_code_list) - 1):
         if i > 0:
-            j = bar_width + bar_width/3
-        plt.axvline(x = axvline_translate + i*(axvline_translate + bar_width + j), color="#DDDDDD")
-
+            j = bar_width + bar_width / 3
+        plt.axvline(x=axvline_translate + i * (axvline_translate + bar_width + j), color="#DDDDDD")
     if program_name:
         program_code_list = [get_program_name(program) for program in program_code_list]
-
-    #setting up x labels and legend
+    # setting up x labels and legend
     ax.set_xticks(x + tick_translate * bar_width)
     ax.set_xticklabels(program_code_list)
-    ax.legend(bbox_to_anchor=(1.04,0), loc="lower left")
-
-    #setting up plot design
+    ax.legend(bbox_to_anchor=(1.04, 0), loc="lower left")
+    # setting up plot design
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
@@ -264,103 +252,123 @@ def plot(subdict, program_code_list, count_dict, title, program_name, image_name
         ax.set_xlabel("Grupos")
     else:
         ax.set_xlabel("Programas")
-
-    #rotating x labels if number of programs/groups is greater than 3
-    #this way labels won't overlap each other
+    # rotating x labels if number of programs/groups is greater than 3
+    # this way labels won't overlap each other
     if len(subdict) > 3:
         fig.autofmt_xdate()
-    
     fig.tight_layout()
-
     if comparing:
-        plt.gca().get_xticklabels()[-1].set_color('red') 
-
+        plt.gca().get_xticklabels()[-1].set_color('red')
     fig.savefig(image_name)
 
-def plot_program(dict_src, program_code_list_str, cluster, image_name=None, comparing=False, program_name=False):
+
+def plot_program(dict_src, program_code_list_str, cluster, image_name=None, comparing=False, program_name=False,
+                 where_to_store="./"):
     program_code_list = string_to_list(program_code_list_str, separator="-")
     subdict = {key: value for key, value in dict_from_json(
         dict_src)[cluster].items() if key in program_code_list}
     count_dict = make_values_dict_from_keys(subdict, ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C/NI/NP"])
-
     if image_name is None:
-        image_name = "../results/" + file_name_from_path(
+        image_name = where_to_store + file_name_from_path(
             dict_src) + "_" + program_code_list_str + ".png"
+    plot(subdict, program_code_list, count_dict, "Produções por programa", program_name, image_name=image_name,
+         comparing=comparing)
 
-    plot(subdict, program_code_list, count_dict, "Produções por programa", program_name, image_name=image_name, comparing=comparing)
 
 def plot_n_best(dict_src, n_str, cluster, compare_to=None, image_name=None, program_name=False):
     n = int(n_str)
     program_code_list_str = "["
     count = 0
-
     for program in dict_from_json(dict_src)[cluster]:
         if count == n:
             break
-
-        if count != n-1:
+        if count != n - 1:
             program_code_list_str += program + '-'
         else:
-            program_code_list_str += program 
+            program_code_list_str += program
             if compare_to is not None:
                 comparing = True
                 program_code_list_str += '-' + compare_to + ']'
             else:
                 comparing = False
                 program_code_list_str += ']'
-
         count += 1
+    plot_program(dict_src, program_code_list_str, cluster, program_name=program_name, image_name=image_name,
+                 comparing=comparing)
 
-    plot_program(dict_src, program_code_list_str, cluster, program_name=program_name, image_name=image_name, comparing=comparing)
 
-def plot_compare(dict_src, program_code_list_str, cluster, image_name=None, program_name=False):
+def plot_compare(dict_src, program_code_list_str, cluster, image_name=None, program_name=False, where_to_store="./"):
     program_code_list = string_to_list(program_code_list_str, separator="-")
     subdict = {key: value for key, value in dict_from_json(
         dict_src)[cluster].items() if key in program_code_list}
     count_dict = make_values_dict_from_keys(subdict, ["A1/A2/A3/A4", "B1/B2/B3/B4", "C/NI/NP"])
-
     if image_name is None:
-        image_name = "../results/" + file_name_from_path(
+        image_name = where_to_store + file_name_from_path(
             dict_src) + "_" + program_code_list_str + "_compare.png"
-    
-    plot(subdict, program_code_list, count_dict, "Comparação entre programas", program_name, image_name=image_name, template="compare")
+    plot(subdict, program_code_list, count_dict, "Comparação entre programas", program_name, image_name=image_name,
+         template="compare")
 
-def plot_program_cluster(dict_src, program, cluster_list_str=None, image_name=None):
+
+def plot_program_cluster(dict_src, program, cluster_list_str=None, image_name=None, where_to_store="./"):
     dictionary = dict_from_json(dict_src)
-    
     if cluster_list_str is not None:
         selected_clusters = string_to_list(cluster_list_str, separator='-')
-    else: 
+    else:
         selected_clusters = dictionary
-        
     subdict = {key: dictionary[key][program] for key in selected_clusters if program in dictionary[key]}
     count_dict = make_values_dict_from_keys(subdict, ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C/NI/NP"])
-
     if image_name is None:
-        image_name = "../results/" + file_name_from_path(
+        image_name = where_to_store + file_name_from_path(
             dict_src) + "_" + program + '_' + ".png"
-
     plot(subdict, subdict.keys(), count_dict, "Publicações por grupo", False, image_name=image_name, cluster=True)
 
-def plot_cluster_compare(dict_src, program, cluster_list_str=None, image_name=None):
+
+def plot_cluster_compare(dict_src, program, cluster_list_str=None, image_name=None, where_to_store="./"):
     dictionary = dict_from_json(dict_src)
-    
     if cluster_list_str is not None:
         selected_clusters = string_to_list(cluster_list_str, separator='-')
-    else: 
+    else:
         selected_clusters = dictionary
-        
     subdict = {key: dictionary[key][program] for key in selected_clusters if program in dictionary[key]}
     count_dict = make_values_dict_from_keys(subdict, ["A1/A2/A3/A4", "B1/B2/B3/B4", "C/NI/NP"])
-
     if image_name is None:
-        image_name = "../results/" + file_name_from_path(
-        dict_src) + "_" + program + '_' + "compare.png"
+        image_name = where_to_store + file_name_from_path(
+            dict_src) + "_" + program + '_' + "compare.png"
+    plot(subdict, subdict.keys(), count_dict, "Comparação entre grupos", False, image_name=image_name,
+         template="compare", cluster=True)
 
-    plot(subdict, subdict.keys(), count_dict, "Comparação entre grupos", False, image_name=image_name, template="compare", cluster=True)
 
-#make_dict("../datasets/Particoes.csv")
-plot_n_best("../results/Particoes.json", 2, "cluster1", "23001011030P1", program_name=True)
-plot_compare("../results/Particoes.json", "[23001011010P0-23001011031P8-23001011020P6]", "cluster1", program_name=True)
-plot_program_cluster("../results/Particoes.json", "23001011010P0", "[cluster1-cluster2]")
-plot_cluster_compare("../results/Particoes.json", "23001011010P0", "[cluster1-cluster2]")
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.main_screen()
+
+    def main_screen(self):
+        self.master.title("Scylax")
+        w = int(self.master.winfo_screenwidth() / 2 - 250)
+        h = int(self.master.winfo_screenheight() / 2 - 250)
+        self.master.geometry("500x500+{}+{}".format(w, h))
+        self.button_load_data = tk.Button(self, text="Load Data", command=self.load_data)
+        self.button_load_data.pack()
+        self.status_label = tk.Label(self)
+        self.status_label.pack()
+
+    def load_data(self):
+        self.status_label["text"] = "Loading..."
+        try:
+            if os.path.exists("./Particoes.json"):
+                if tk.messagebox.askquestion ("This data has been imported before", "Would you like to import anyway?") == "no":
+                    self.status_label["text"] = "Finished!"
+                    return
+            file_name = filedialog.askopenfilename(initialdir=".", title="Select file to load data", filetypes=(("csv files", "*.csv"), ("all files", "*.*")))
+            make_dict(file_name)
+            self.status_label["text"] = "Finished!"
+        except:
+            self.status_label["text"] = "Fail!"
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
