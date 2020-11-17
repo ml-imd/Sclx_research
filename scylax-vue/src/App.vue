116411b6c14c8658896d68a0c342cbb0d97b94f5
mateusfb <mateusfirminob@gmail.com>
2020-11-17 18:16:20 -0300

    Adicionada gaveta lateral com menu

--- scylax-vue/src/App.vue
+++ scylax-vue/src/App.vue
@@ -1,60 +1,50 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
+    <v-navigation-drawer
+      permanent
+      expand-on-hover
+      color="#58A4B0"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-menu</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Menu</v-list-item-title>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item 
+          v-for="(item, index) in menuItems" 
+          :key="index"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    menuItems: [
+      {title: "Redes de colaboração", icon: "mdi-lan"},
+      {title: "Programas de pós-graduação", icon: "mdi-bank"},
+      {title: "Pesquisadores", icon: "mdi-account"}
+    ],
   }),
 };
 </script>
