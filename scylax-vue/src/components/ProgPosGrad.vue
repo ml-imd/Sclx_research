bb0111917330880faddfffc4afa1a550b894389d
mateusfb <mateusfirminob@gmail.com>
2020-11-19 21:52:49 -0300

    Mudanças gráficas na tela de redes de colaboração

--- scylax-vue/src/components/ProgPosGrad.vue
+++ scylax-vue/src/components/ProgPosGrad.vue
@@ -3,7 +3,14 @@
         fluid
         class="pa-6"
     >
-        
+        <v-text-field
+            color="#58A4B0"
+            background-color="white"
+            placeholder="Número de grupos"
+            filled
+            type="number"
+            hide details
+        ></v-text-field>
     </v-container>
 </template>
 
