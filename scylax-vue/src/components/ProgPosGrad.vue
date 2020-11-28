35301d41cd4ef17e9e714237fcefe794d49ed113
mateusfb <mateusfirminob@gmail.com>
2020-11-28 17:08:36 -0300

    Adicionando mais gráficos

--- scylax-vue/src/components/ProgPosGrad.vue
+++ scylax-vue/src/components/ProgPosGrad.vue
@@ -68,18 +68,47 @@
         </v-row>
         <v-row>
             <v-col>
-                <v-card v-if="canRender" width="100%">
+                <v-card v-if="canRender" width="100%" color="#fafbfd">
                     <v-container>
                         <v-row>
                             <v-col>
-                                <iframe :src="cluster_scores" height="400" width="100%" frameBorder="0"></iframe>
+                                <h3 class="pl-5">Programas por grupo</h3>
+                                <iframe :src="prog_per_cluster" height="300" width="100%" frameBorder="0"></iframe>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
+                                <h3 class="pl-5">Publicações por grupo por ano</h3>
+                                <iframe :src="pub_by_year" height="500" width="100%" frameBorder="0"></iframe>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <h3 class="pl-5">Citescore médio por grupo</h3>
+                                <iframe :src="citescore_average" height="500" width="100%" frameBorder="0"></iframe>
+                            </v-col>
+                            <v-col>
+                                <h3 class="pl-5">Quartil citescore médio por grupo</h3>
+                                <iframe :src="quartil_citescore_average" height="500" width="100%" frameBorder="0"></iframe>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <h3 class="pl-5">Percentil médio por grupo</h3>
+                                <iframe :src="percentile_average" height="500" width="100%" frameBorder="0"></iframe>
+                            </v-col>
+                            <v-col>
+                                <h3 class="pl-5">Quartil percentil médio por grupo</h3>
+                                <iframe :src="quartil_percentile_average" height="500" width="100%" frameBorder="0"></iframe>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col>
+                                <h3 class="pl-5">Publicações por grupo por qualis</h3>
                                 <iframe :src="cluster_qualis" height="500" width="100%" frameBorder="0"></iframe>
                             </v-col>
                             <v-col>
+                                <h3 class="pl-5">Qualidade de publicações por grupo</h3>
                                 <iframe :src="cluster_qualis_gsb" height="500" width="100%" frameBorder="0"></iframe>
                             </v-col>
                         </v-row>
@@ -106,7 +135,7 @@
                                 <v-col>
                                     <v-select
                                         :items="selectionProgramClusters"
-                                        v-model="mainProgramCluster"
+                                        v-model="groupSelect"
                                         label="Grupo"
                                         filled
                                         color="#424C63"
@@ -130,6 +159,7 @@
                                         large
                                         height="55"
                                         @click="updateProgramGraphs"
+                                        :disabled="(groupSelect == '') || (mainProgram == '')"
                                     >
                                         Analisar Programa 
                                         <v-icon >mdi-menu-right</v-icon>
@@ -142,10 +172,17 @@
                         <div v-if="canRenderProgram">
                             <v-row>
                                 <v-col>
-                                    <iframe id="program_qualis" :src="program_qualis" height="600" width="100%" frameBorder="0"></iframe>
+                                    <iframe :src="program_citescore" height="500" width="100%" frameBorder="0"></iframe>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col>
+                                    <h3 class="pl-5">Publicações por qualis por programa</h3>
+                                    <iframe :src="program_qualis" height="500" width="100%" frameBorder="0"></iframe>
                                 </v-col>
                                 <v-col>
-                                    <iframe id="program_qualis_sgb" :src="program_qualis_gsb" height="600" width="100%" frameBorder="0"></iframe>
+                                    <h3 class="pl-5">Qualidade de publicações por programa</h3>
+                                    <iframe :src="program_qualis_gsb" height="500" width="100%" frameBorder="0"></iframe>
                                 </v-col>
                             </v-row>
                         </div>
@@ -161,6 +198,7 @@ export default {
     name: "ProgPosGrad",
 
     data: () => ({
+        groupSelect: "",
         numberOfClusters: 2,
         canRender: false,
         clusterUrlId: "",
@@ -194,6 +232,8 @@ export default {
                 this.canRenderProgram = true;
             }
 
+            this.mainProgramCluster = this.groupSelect;
+
             this.urlProgramList = "(";
             this.urlProgramList += "(input:(language:kuery,query:'programas_nomes.keyword:" + this.mainProgram + "'),label:'" + this.mainProgram + "')";
             this.urlProgramList += ")";
@@ -209,8 +249,20 @@ export default {
             return "http://localhost:5601/app/visualize#/edit/3a2a3670-2f56-11eb-b8d7-2bd9025fb19b?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(vis:(colors:(A1:%23508642,A2:%239AC48A,A3:%23CCA300,A4:%23F2C96D,B1:%23E0752D,B2:%23E24D42,B3:%23890F02,B4:%230A437C,Bronze:%2399440A,C%2FNI%2FNP:%233F2B5B,Gold:%23E5AC0E,Silver:%23DEDAF7))),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:Quantidade),schema:metric,type:count),(enabled:!t,id:'2',params:(customLabel:Programa,field:" + this.clusterUrlId + ".keyword,include:'',missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:'1',otherBucket:!f,otherBucketLabel:Other,size:5),schema:segment,type:terms),(enabled:!t,id:'3',params:(filters:!((input:(language:kuery,query:'qualis.keyword%20:%20%22A1%22%20or%20qualis.keyword%20%0A:%20%22A2%22%20or%20qualis.keyword%20:%20%22A3%22%20or%20qualis.keyword%20:%20%22A4%22'),label:Gold),(input:(language:kuery,query:'qualis.keyword%20:%20%22B1%22%20or%20qualis.keyword%20:%20%22B3%22%20or%20qualis.keyword%20:%20%22B4%22%20or%20qualis.keyword%20:%20%22B2%22%20'),label:Silver),(input:(language:kuery,query:'qualis.keyword%20:%20%22C%2FNI%2FNP%22%20'),label:Bronze))),schema:group,type:filters)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!t,rotate:0,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f,valueAxis:ValueAxis-1),labels:(show:!f),legendPosition:right,seriesParams:!((data:(id:'1',label:Quantidade),drawLinesBetweenPoints:!t,lineWidth:2,mode:normal,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:histogram,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(mode:normal,type:linear),show:!t,style:(),title:(text:Quantidade),type:value))),title:cluster_qualis_gsb,type:histogram))"
         },
 
-        cluster_scores: function(){
-            return "http://localhost:5601/app/visualize#/edit/1eca5170-2ec6-11eb-a9e4-f3fe9303f5c9?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'Citescore%20m%C3%A9dio',field:citescore),schema:metric,type:avg),(enabled:!t,id:'2',params:(customLabel:Grupo,field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:'1',otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:average_citescore_metric,type:metric))"
+        citescore_average: function(){
+            return "http://localhost:5601/app/visualize#/edit/1eca5170-2ec6-11eb-a9e4-f3fe9303f5c9?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'Citescore',field:citescore),schema:metric,type:avg),(enabled:!t,id:'2',params:(customLabel:Grupo,field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:asc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:average_citescore_metric,type:metric))"
+        },
+
+        quartil_citescore_average: function(){
+            return "http://localhost:5601/app/visualize#/edit/1eca5170-2ec6-11eb-a9e4-f3fe9303f5c9?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'Quartil%20citescore',field:quartil_citescore),schema:metric,type:avg),(enabled:!t,id:'2',params:(customLabel:Grupo,field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:asc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:average_citescore_metric,type:metric))"
+        },
+
+        percentile_average: function(){
+            return "http://localhost:5601/app/visualize#/edit/1eca5170-2ec6-11eb-a9e4-f3fe9303f5c9?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:Percentil,field:percentile),schema:metric,type:avg),(enabled:!t,id:'2',params:(customLabel:Grupo,field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:asc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:average_citescore_metric,type:metric))"
+        },
+
+        quartil_percentile_average: function(){
+            return "http://localhost:5601/app/visualize#/edit/1eca5170-2ec6-11eb-a9e4-f3fe9303f5c9?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:'Quartil%20percentil',field:quartil_percentile),schema:metric,type:avg),(enabled:!t,id:'2',params:(customLabel:Grupo,field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:asc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:average_citescore_metric,type:metric))"
         },
 
         program_qualis: function(){
@@ -219,6 +271,18 @@ export default {
 
         program_qualis_gsb: function(){
             return "http://localhost:5601/app/visualize#/edit/c78238c0-2ec4-11eb-a9e4-f3fe9303f5c9?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'0922d430-2ec3-11eb-a9e4-f3fe9303f5c9',key:" + this.clusterUrlId + ".keyword,negate:!f,params:(query:" + this.mainProgramCluster + "),type:phrase),query:(match_phrase:(" + this.clusterUrlId + ".keyword:" + this.mainProgramCluster + ")))),linked:!f,query:(language:kuery,query:''),uiState:(vis:(colors:(A1:%23508642,A2:%239AC48A,A3:%23CCA300,A4:%23F2C96D,B1:%23E0752D,B2:%23E24D42,B3:%23890F02,B4:%230A437C,Bronze:%2399440A,C%2FNI%2FNP:%233F2B5B,Gold:%23E5AC0E,Silver:%23DEDAF7))),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:Quantidade),schema:metric,type:count),(enabled:!t,id:'2',params:(filters:!" + this.urlProgramList + "),schema:segment,type:filters),(enabled:!t,id:'3',params:(filters:!((input:(language:kuery,query:'qualis.keyword%20:%20%22A1%22%20or%20qualis.keyword%20%0A:%20%22A2%22%20or%20qualis.keyword%20:%20%22A3%22%20or%20qualis.keyword%20:%20%22A4%22'),label:Gold),(input:(language:kuery,query:'qualis.keyword%20:%20%22B1%22%20or%20qualis.keyword%20:%20%22B3%22%20or%20qualis.keyword%20:%20%22B4%22%20or%20qualis.keyword%20:%20%22B2%22%20'),label:Silver),(input:(language:kuery,query:'qualis.keyword%20:%20%22C%2FNI%2FNP%22%20'),label:Bronze))),schema:group,type:filters)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!t,rotate:0,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f,valueAxis:ValueAxis-1),labels:(show:!f),legendPosition:right,seriesParams:!((data:(id:'1',label:Quantidade),drawLinesBetweenPoints:!t,lineWidth:2,mode:normal,show:!t,showCircles:!t,type:histogram,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:histogram,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(mode:normal,type:linear),show:!t,style:(),title:(text:Quantidade),type:value))),title:program_qualis_gsb,type:histogram))"
+        },
+        
+        pub_by_year: function(){
+            return "http://localhost:5601/app/visualize#/create?embed=true&type=line&indexPattern=0922d430-2ec3-11eb-a9e4-f3fe9303f5c9&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(vis:(colors:(cluster1:%23447EBC))),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:Quantidade),schema:metric,type:count),(enabled:!t,id:'2',params:(customLabel:Ano,field:ano.keyword,missingBucket:!f,missingBucketLabel:Missing,order:asc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:100),schema:segment,type:terms),(enabled:!t,id:'3',params:(customLabel:Grupo,field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:desc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!t,addTimeMarker:!f,addTooltip:!t,categoryAxes:!((id:CategoryAxis-1,labels:(filter:!t,show:!t,truncate:100),position:bottom,scale:(type:linear),show:!t,style:(),title:(),type:category)),grid:(categoryLines:!f,valueAxis:ValueAxis-1),labels:(),legendPosition:right,orderBucketsBySum:!f,seriesParams:!((data:(id:'1',label:Quantidade),drawLinesBetweenPoints:!t,interpolate:linear,lineWidth:2,mode:normal,show:!t,showCircles:!t,type:line,valueAxis:ValueAxis-1)),thresholdLine:(color:%23E7664C,show:!f,style:full,value:10,width:1),times:!(),type:line,valueAxes:!((id:ValueAxis-1,labels:(filter:!f,rotate:0,show:!t,truncate:100),name:LeftAxis-1,position:left,scale:(defaultYExtents:!f,mode:normal,setYExtents:!f,type:linear),show:!t,style:(),title:(text:Quantidade),type:value))),title:'',type:line))"
+        },
+
+        prog_per_cluster: function(){
+            return "http://localhost:5601/app/visualize#/create?embed=true&type=metric&indexPattern=0922d430-2ec3-11eb-a9e4-f3fe9303f5c9&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'1',params:(customLabel:Programas,field:programas.keyword),schema:metric,type:cardinality),(enabled:!t,id:'2',params:(field:" + this.clusterUrlId + ".keyword,missingBucket:!f,missingBucketLabel:Missing,order:asc,orderBy:_key,otherBucket:!f,otherBucketLabel:Other,size:5),schema:group,type:terms)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:'',type:metric))"
+        },
+
+        program_citescore: function(){
+            return "http://localhost:5601/app/visualize#/edit/c2a69990-31b2-11eb-8045-b53bd470100c?embed=true&type=metric&indexPattern=0922d430-2ec3-11eb-a9e4-f3fe9303f5c9&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'0922d430-2ec3-11eb-a9e4-f3fe9303f5c9',key:" + this.clusterUrlId + ".keyword,negate:!f,params:(query:" + this.mainProgramCluster + "),type:phrase),query:(match_phrase:(" + this.clusterUrlId + ".keyword:" + this.mainProgramCluster + ")))),linked:!f,query:(language:kuery,query:''),uiState:(),vis:(aggs:!((enabled:!t,id:'3',params:(customLabel:'Citescore%20m%C3%ADnimo',field:citescore),schema:metric,type:min),(enabled:!t,id:'1',params:(customLabel:'Citescore%20m%C3%A9dio',field:citescore),schema:metric,type:avg),(enabled:!t,id:'2',params:(filters:!" + this.urlProgramList + "),schema:group,type:filters),(enabled:!t,id:'4',params:(customLabel:'Citescore%20m%C3%A1ximo',field:citescore),schema:metric,type:max)),params:(addLegend:!f,addTooltip:!t,metric:(colorSchema:'Green%20to%20Red',colorsRange:!((from:0,to:10000)),invertColors:!f,labels:(show:!t),metricColorMode:None,percentageMode:!f,style:(bgColor:!f,bgFill:%23000,fontSize:60,labelColor:!f,subText:''),useRanges:!f),type:metric),title:program_citescore_metric,type:metric))"
         }
     }
 }
