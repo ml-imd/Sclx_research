e583bbd8e44d6278b16e364e6b34df76aee36ac6
mateusfb <mateusfirminob@gmail.com>
2022-06-11 16:37:23 -0300

    Remove SpectralClustering e adiciona EM

--- analyze_csv/src/Script.py
+++ analyze_csv/src/Script.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.cluster import *
+from sklearn.mixture import GaussianMixture
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import davies_bouldin_score
 from ntpath import split, basename
@@ -43,7 +44,8 @@ def cluster(method, parameters = {}):
                "OPTICS" : OPTICS,
                "SpectralClustering" : SpectralClustering,
                "SpectralBiclustering" : SpectralBiclustering,
-               "SpectralCoclustering" : SpectralCoclustering,}
+               "SpectralCoclustering" : SpectralCoclustering,
+               "GaussianMixture": GaussianMixture,}
                
     return methods[method](**parameters)
 
@@ -72,8 +74,11 @@ def writeData(file_name, method, slt_score, db_score, k = '-', seed = '-'):
     file = open(file_name, 'a')
     if(stat(file_name).st_size == 0):
         file.write("method,n_cluster,seed,silhoutte,db\n")
+        print("method,n_cluster,seed,silhoutte,db\n")
     
     file.write(method + ',' + k + ',' + seed + ',' + slt_score + ',' + db_score + '\n')
+    print(method + ',' + k + ',' + seed + ',' + slt_score + ',' + db_score + '\n')
+    
     file.close()
 
 
@@ -86,6 +91,8 @@ def analyze(path, method, file_name, parameters = {}, k_range = None, seed_value
         for x in range(k_range[0], k_range[1]):
             if(method == "SpectralBiclustering"):
                 parameters["n_clusters"] = (x, dataset.shape[1])
+            elif(method == "GaussianMixture"):
+                parameters["n_components"] = x;
             else:
                 parameters["n_clusters"] = x
             for y in seed_values:
@@ -94,6 +101,8 @@ def analyze(path, method, file_name, parameters = {}, k_range = None, seed_value
                 cluster_method.fit(dataset)
                 if(method == "SpectralBiclustering" or method == "SpectralCoclustering"):
                     scores = getScores(dataset, cluster_method.row_labels_)
+                elif(method == "GaussianMixture"):
+                    scores = getScores(dataset, cluster_method.predict(dataset))
                 else:
                     scores = getScores(dataset, cluster_method.labels_)
                 writeData(file_name, method, k = str(x), seed = str(y), slt_score = str(scores[0]), db_score = str(scores[1]))
@@ -155,7 +164,7 @@ def analyzeAll(path, k_range, seed_values, label_col = None):
     file_name = fileNameFromPath(path) + '_' + 'k' + str(k_range) +'_' + 'seed' + str(seed_values) + '.csv'
     have_none = ["AffinityPropagation", "DBSCAN", "OPTICS", "MeanShift"]
     have_k = ["AgglomerativeClustering", "Birch"]
-    have_seed_and_k = ["KMeans", "MiniBatchKMeans", "SpectralClustering", "SpectralBiclustering", "SpectralCoclustering"]
+    have_seed_and_k = ["KMeans", "MiniBatchKMeans", "SpectralBiclustering", "SpectralCoclustering", "GaussianMixture"]
     
     for method in have_seed_and_k:
         analyze(path, method, file_name, k_range = k_range, seed_values = seed_values, label_col = label_col)
